ed1309da0dd4f30f0896c70d2a034aecf09e2b6d
小梦 <yyddtj@yyddtj.top>
2026-09-03 02:35:56 +0800

    Add files via upload

---
+++ assets/js/ads.js
@@ -0,0 +1,15 @@
+(function(){
+  const slot=document.querySelector('[data-ad-slot]');
+  if(!slot)return;
+  const external=document.createElement('script');
+  external.src='https://evacuateenclose.com/d0/d7/2a/d0d72a74881daf09b3e464d5496c0686.js';
+  slot.appendChild(external);
+  const invoke=document.createElement('script');
+  invoke.async=true;
+  invoke.dataset.cfasync='false';
+  invoke.src='https://evacuateenclose.com/59dc92d7cc148e91122dcb8052d20b2c/invoke.js';
+  slot.appendChild(invoke);
+  const container=document.createElement('div');
+  container.id='container-59dc92d7cc148e91122dcb8052d20b2c';
+  slot.appendChild(container);
+})();
